fa0065747b874e42d7e9963ccc229596d3d92315
alexandraiosib <alexandra.iosib@gmail.com>
2021-12-14 15:50:33 +0200

    Add project files.

---
+++ Iosib_Alexandra_Lab10/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Iosib_Alexandra_Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
